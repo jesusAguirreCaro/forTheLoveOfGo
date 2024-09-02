40060c875ac3bf959dec070b85b25e7b9222c363
Jesus Aguirre <jesus.aguirre095@gmail.com>
2024-09-02 12:22:42 -0600

    chapter 1

---
+++ chapter 1/calculator/calculator.go
@@ -0,0 +1,12 @@
+// Package calculator does simple calculations.
+package calculator
+
+// Add takes two numbers and returns the result of adding
+// them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Subtract(a, b float64) float64 {
+	return a - b
+}
